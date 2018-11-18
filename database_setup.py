1abca09a920366c57dd594d5658b9128f1863359
Sakshi Singh <30416637+SakshiSSingh@users.noreply.github.com>
2018-11-18 23:18:43 +0530

    Update database_setup.py

--- database_setup.py
+++ database_setup.py
@@ -19,7 +19,6 @@ class User(Base):
 
 class Category(Base):
     __tablename__ = 'category'
-    items = relationship("Item", cascade="all, delete-orphan")
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     user_id = Column(Integer, ForeignKey('user.id'))
@@ -36,8 +35,6 @@ class Category(Base):
 class Item(Base):
     __tablename__ = 'item'
 
-    category = relationship("Category", backref=backref
-                            ("items", cascade="all, delete-orphan"))
     name = Column(String(80), nullable=False)
     id = Column(Integer, primary_key=True)
     description = Column(String(250))
